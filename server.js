1238a8046c2b3821240103633bc7b347f5eebda4
noname01 <hjc.noname@gmail.com>
2014-07-20 13:16:03 -0700

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+
+var express = require("express");
+var app = express();
+
+app.set("view engine", "jade")
+
+//static middleware
+var oneDay = 86400000;
+app.use(express.compress());
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+var server = app.listen(3000, function(){
+  console.log("Listening on port %d", server.address().port);
+});
