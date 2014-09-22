0b5b21e22cbde3775edd4bb936149cb1660fe255
noname01 <hjc.noname@gmail.com>
2014-09-22 14:03:18 -0700

    img carousel

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
-   $(window).scroll(function(){
-       if($(window).scrollTop() > 70) $(".navbar, .nav-content").addClass("invert");
-       else $(".navbar, .nav-content").removeClass("invert");
-   })
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 70) $(".navbar, .nav-content").addClass("invert");
+        else $(".navbar, .nav-content").removeClass("invert");
+    });
+    $(document).ready(function(){
+        $('.slick').slick({
+            lazyLoad: 'ondemand',
+            infinite: true,
+            slidesToShow: 4,
+            autoplay: true,
+            autoplaySpeed: 2000
+        });
+    })
 });
