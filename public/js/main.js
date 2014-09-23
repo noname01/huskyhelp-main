95028230ba9503dad62eaae6c81718d6c83e44b7
noname01 <hjc.noname@gmail.com>
2014-09-23 15:40:33 -0700

    style fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
 $(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 70) $(".navbar, .nav-content").addClass("invert");
-        else $(".navbar, .nav-content").removeClass("invert");
-    });
+    if(!$(".navbar").hasClass("navbar-fallback")){
+        $(window).scroll(function(){
+            if($(window).scrollTop() > 70) $(".navbar, .nav-content").addClass("invert");
+            else $(".navbar, .nav-content").removeClass("invert");
+        });
+    }
     $(document).ready(function(){
         $('.slick').slick({
             lazyLoad: 'ondemand',
             infinite: true,
             slidesToShow: 4,
             autoplay: true,
-            autoplaySpeed: 2000
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }
+            ]
         });
     })
 });
